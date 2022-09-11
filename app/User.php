3dee7262a2d1ab5543ccba027bd2a6b5144e770d
sayemalmahdi <sayemalmahdi@gmail.com>
2022-09-11 13:37:26 +0600

    Multi-Auth Part-1 Done

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name','username','phone', 'email', 'password', 'provider', 'provider_id',
     ];
 
     /**
@@ -36,4 +36,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
 }
