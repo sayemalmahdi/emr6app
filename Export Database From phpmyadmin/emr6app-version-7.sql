65efaeaba9e7dd21c0662dc4d20773291156bd2f
sayemalmahdi <sayemalmahdi@gmail.com>
2022-09-15 18:22:39 +0600

    Add a Database File called 'emr6app-for-version-7\.sql'

---
+++ Export Database From phpmyadmin/emr6app-version-7.sql
@@ -0,0 +1,295 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 15, 2022 at 02:21 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `emr6app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2020_03_27_073538_create_roles_table', 1),
+(5, '2020_03_30_083610_create_categories_table', 2),
+(6, '2020_03_30_083906_create_subcategories_table', 2),
+(7, '2020_03_30_085515_create_districts_table', 2),
+(8, '2020_06_04_071022_create_subdistricts_table', 3),
+(9, '2020_06_05_034444_create_news_tabel', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescriptions`
+--
+
+CREATE TABLE `prescriptions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `firstname` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastname` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dob` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `patient_weight` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `blood_pressure` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pulse` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `patient_complaint` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `notes` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `duration` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `investigation_name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `instrustion` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `diagnosis_name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `comment` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `visit_after_days` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `medicines` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `prescriptions`
+--
+
+INSERT INTO `prescriptions` (`id`, `firstname`, `lastname`, `phone`, `dob`, `patient_weight`, `blood_pressure`, `pulse`, `patient_complaint`, `notes`, `duration`, `investigation_name`, `instrustion`, `diagnosis_name`, `comment`, `visit_after_days`, `medicines`, `status`, `created_at`, `updated_at`) VALUES
+(4, 'Sayem update 1', 'Al-Mahdi', '01772235315', '03/10/1997', '65', '12', '10', 'xxxxxx', 'notesxxxx', '10', 'Inxxx', 'Insxxx', 'xxx', 'cxxx', '4', 'Napa', 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin', NULL, NULL),
+(2, 'Author', 'author', NULL, NULL),
+(3, 'User', 'user', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site_setting`
+--
+
+CREATE TABLE `site_setting` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `phone_one` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_two` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `company_name_en` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `company_name_bn` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `company_address` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `facebook` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `youtube` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `instagram` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `twitter` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `site_setting`
+--
+
+INSERT INTO `site_setting` (`id`, `phone_one`, `phone_two`, `email`, `company_name_en`, `company_name_bn`, `company_address`, `facebook`, `youtube`, `instagram`, `twitter`, `created_at`, `updated_at`) VALUES
+(1, '01772235315', '01858327366', 'sayem.mahdi@sjinnovation.com', 'SJ EMR', 'এসজে ইএমআর', 'Uposhohor Block-F', 'https://www.facebook.com/', NULL, 'https://www.instagram.com/', 'https://twitter.com/', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `role_id` int(11) NOT NULL DEFAULT 3,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `about` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `type` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `admins` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `settings` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `prescriptions` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role_id`, `name`, `phone`, `username`, `email`, `email_verified_at`, `password`, `image`, `about`, `remember_token`, `created_at`, `updated_at`, `type`, `admins`, `settings`, `prescriptions`) VALUES
+(1, 1, 'Md.Admin', NULL, 'admin', 'admin@gmail.com', NULL, '$2y$10$mvkj.v/FqAewIVOO5DsNvuROLGRVbJDgjswttmmseBv/kKzozCJDa', NULL, NULL, NULL, NULL, NULL, NULL, '1', '1', NULL),
+(2, 2, 'Md.Author', NULL, 'author', 'author@gmail.com', NULL, '$2y$10$B9qEjl6WJ77sHXaR3J//peh/zoCspgoPfyxEf4Fgcn0UejcP5LGSq', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 3, 'Md.User', NULL, 'user', 'user@gmail.com', NULL, '$2y$10$V3Hbvb4AsABcXmwKxOnvGuVgj.GKttZYoKT0QU5YCUlW/8fxLO/QK', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(14, 1, 'Doctor Sayem', '01772235315', 'dr.sayem', 'dr.sayem@gmail.com', NULL, '$2y$10$3WrAiGLfqszcpzTJuFSl3.JvPv4gv/R3OomtHrQ5eAYxgNZWLtNym', NULL, NULL, NULL, NULL, NULL, '2', '1', '1', '1'),
+(15, 1, 'Doctor Sumon Paul', '01882235315', 'dr.sumon', 'dr.sumon@gmail.com', NULL, '$2y$10$7tsD4zoxVsUrEoaE1mQiVeQihdGqUkwWJTuNte02Q3vgtyw8rcTNy', NULL, NULL, NULL, NULL, NULL, '2', NULL, '1', '1'),
+(17, 1, 'Doctor Asif', '01772235315', 'dr.asif', 'dr.asif@gmail.com', NULL, '$2y$10$9ewfJCp5vGWdJFLmkqE.Z.mK0hz.WnMplXJlNWZaQlviQGcTfqtue', NULL, NULL, NULL, NULL, NULL, '2', NULL, '1', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `prescriptions`
+--
+ALTER TABLE `prescriptions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `site_setting`
+--
+ALTER TABLE `site_setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_username_unique` (`username`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `prescriptions`
+--
+ALTER TABLE `prescriptions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `site_setting`
+--
+ALTER TABLE `site_setting`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
